cb0ed33c79da0ae57e50f4a9a15480e41d2a4bc7
Mario Valenzuela <mariovalpa@gmail.com>
2018-08-27 08:14:31 -0600

    Se agrega validación de valor de As en cálculo de puntos del jugador, puede ser 1 u 11 dependiendo del total

--- src/mcc/itculiacan/blackjack/model/Player.java
+++ src/mcc/itculiacan/blackjack/model/Player.java
@@ -20,10 +20,16 @@ public class Player {
     public int getHandValue() {
 
         int value = 0;
+        boolean hasAs = false;
 
         for(Card card: hand) {
+            if (card == Card.ClubsA || card == Card.DiamondsA || card == Card.HearthsA || card == Card.SpadesA)
+                hasAs = true;
             value += card.getValue();
         }
+
+        if ( hasAs && (value+10) <= 21)
+            value += 10;
 
         return value;
     }
